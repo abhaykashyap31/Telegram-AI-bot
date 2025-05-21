087b81a61996cbc1988743570bfc59c4f6cc277c
abhaykashyap31 <abhikashyap4563@gmail.com>
2025-05-21 20:03:37 +0530

    FInal

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import uvicorn
 from main import main
 
 def run_fastapi():
-    uvicorn.run("main:main", host="127.0.0.1", port=8000, log_level="info")
+    uvicorn.run("main:main", host="0.0.0.0", port=8000, log_level="info")
 
 if __name__ == "__main__":
     threading.Thread(target=run_fastapi, daemon=True).start()
